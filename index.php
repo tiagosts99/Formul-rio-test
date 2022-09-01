e9983ac7b36811a4b149ee5bd985fb0a5373fe45
Tiago Samuel Sousa dos Santos <tiago.samuel@unisantos.br>
2022-09-01 17:27:31 -0300

    new

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php include_once("Formulario.html");
+<?php include_once("Formulario.html"); //Irá adicionar o arquivo especificado na execução do script, porém só irá incluir uma unica vez.
 
 
 ?>
